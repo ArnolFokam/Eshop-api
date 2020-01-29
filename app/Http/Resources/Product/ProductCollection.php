65daf15cc1742ecd85f192c2c340210ef2021e04
Arnol Fokam <arnolfokam23@gmail.com>
2020-01-29 01:21:51 +0100

    Added collection to products

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -3,8 +3,10 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +16,13 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count('star') > 0 ? round($this->reviews->sum('star')/$this->reviews->count('star'), 1) : 0,
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
     }
 }
